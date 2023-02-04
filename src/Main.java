37d53128689a66d8cbd2c50b03cd6de2b95eaff7
Valeriy Kulagin <kulagin.valeriy@gmail.com>
2023-02-04 12:17:50 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 75;
+        int bonusAmount = 20;
+
+        int bonusMiles;
+        if (amount >= bonusAmount) {
+            bonusMiles = amount / bonusAmount;
+        } else {
+            bonusMiles = 0;
+        }
+        System.out.println(bonusMiles);
+    }
+}
